40b2264fd0f142eba8625598ec0dbab846e0ddf2
Jooyon Kim <gina611.kaist@gmail.com>
2019-08-12 23:58:19 +0900

    Add main urls

--- jjanhaeyo/main/urls.py
+++ jjanhaeyo/main/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+admin.autodiscover()
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title='jjanhaeyo API',
+      default_version='v1',
+   ),
+   public=False,
+)
 
 urlpatterns = [
+    path('accounts/', include('accounts.urls')),
+    # path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if 'debug_toolbar' in settings.INSTALLED_APPS:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
